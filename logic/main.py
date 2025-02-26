f8c6c225fe67bf9bec1df335310db27a4e2c14ad
girvel <widauka@ya.ru>
2025-02-26 15:05:41 +0500

    Move the code around

--- logic/main.py
+++ logic/main.py
@@ -2,18 +2,33 @@ import psycopg
 
 
 if __name__ == '__main__':
-    conn = psycopg.connect(
+    with psycopg.connect(
         dbname="testdb",
         user="postgres",
         password="postgres",
         host="localhost",
         port=5432,
-    )
-    print("Connected")
+    ) as conn:
+        print("Connected")
 
-    cursor = conn.cursor()
-    cursor.execute("SELECT version();")
-    db_version = cursor.fetchone()
-    print(f"{db_version = }")
+        db_version, = conn.execute("SELECT version();").fetchone()
+        print(f"{db_version = }")
 
-    conn.close()
+        already_exists = conn.execute("""
+            SELECT EXISTS(
+                SELECT *
+                FROM information_schema.tables
+                WHERE table_name='users'
+            )
+        """).fetchone()[0]
+
+        if not already_exists:
+            conn.execute("""
+                CREATE TABLE users (
+                    user_id SERIAL PRIMARY KEY,
+                    name varchar(255)
+                );
+            """)
+
+        conn.execute("INSERT INTO users (name) VALUES (%s)", ("Demo Demoviev", ))
+        print(conn.execute("SELECT user_id, name FROM users").fetchall())
