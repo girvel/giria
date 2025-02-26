9743a2d98c1d17d55f3334761ef3c5d24d77b850
girvel <widauka@ya.ru>
2025-02-26 16:14:57 +0500

    Connect (non-async) API to DB

--- api/main.py
+++ api/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+import psycopg
 
 app = FastAPI()
+db = psycopg.Connection.connect(
+    dbname="testdb",
+    user="postgres",
+    password="postgres",
+    host="db",
+    port=5432,
+)
 
 
 @app.get("/")
 def hello_world():
     return {"Hello": "World1"}
+
+# TODO! async
+# TODO! return type
+@app.get("/global_map")
+def global_map():
+    with db.cursor() as cursor:
+        return cursor.execute("""SELECT * FROM global_map""").fetchall()
