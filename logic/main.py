a26c90771e271dfbc4cfbe34888d616cae16c426
girvel <widauka@ya.ru>
2025-03-03 17:22:30 +0500

    Add players & cities to the DB

--- logic/main.py
+++ logic/main.py
@@ -26,10 +26,29 @@ if __name__ == '__main__':
 
         if not already_exists:
             conn.execute("""
+                CREATE TABLE players (
+                    player_id SERIAL PRIMARY KEY,
+                    login TEXT NOT NULL
+                );
+                
+                INSERT INTO players (login)
+                VALUES ('girvel');
+                
+                CREATE TABLE cities (
+                    city_id SERIAL PRIMARY KEY,
+                    population INTEGER NOT NULL,
+                    player_id INTEGER REFERENCES players
+                );
+                
+                INSERT INTO cities (population, player_id)
+                VALUES (1000, 1);
+                
                 CREATE TABLE global_map (
                     x INTEGER NOT NULL,
                     y INTEGER NOT NULL,
-                    tile VARCHAR(8)
+                    tile VARCHAR(8),
+                    city_id INTEGER REFERENCES cities,
+                    PRIMARY KEY(x, y)
                 );
             """)
 
@@ -46,6 +65,13 @@ if __name__ == '__main__':
                         "INSERT INTO global_map (x, y, tile) VALUES (%s, %s, %s)",
                         (x, y, TILE_TYPES[character])
                     )
+
+            conn.execute("""
+                UPDATE global_map
+                SET city_id = 1
+                WHERE x = 1 AND y = 1;
+            """)
+
 
             print("Initialized DB")
 
